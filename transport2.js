78f351420659e8530ee69d2484eeca8b3047ca4e
Paulo Toureiro <51710865+spicymanp@users.noreply.github.com>
2024-05-09 09:59:29 +0200

    Changed fs from asyncronous to syncronous.

--- transport2.js
+++ transport2.js
@@ -3,19 +3,26 @@ import path from "path";
 
 const sptRoot = "H:\\EFT-SPT\\user\\mods";
 const modsWithBundles = [];
-fs.readdir(sptRoot, (err, items) => {
-  for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-    const fullPath = path.join(sptRoot, item);
 
-    const fullPathWithBundles = path.join(fullPath, "bundles");
-    const hasBundles = fs.existsSync(fullPathWithBundles);
-    console.log(fullPath + "  -----  " + hasBundles);
-    if (hasBundles) {
-      modsWithBundles.push(fullPathWithBundles);
-    }
-  }
+const mods = fs.readdirSync(sptRoot);
+console.log("\nCurrently Installed Mods:\n ");
+mods.forEach((mod) => {
+  console.log("- " + mod);
 });
 
-console.log("fuck you");
-console.log(modsWithBundles.length);
+// fs.readdir(sptRoot, (err, items) => {
+//   for (let i = 0; i < items.length; i++) {
+//     const item = items[i];
+//     const fullPath = path.join(sptRoot, item);
+
+//     const fullPathWithBundles = path.join(fullPath, "bundles");
+//     const hasBundles = fs.existsSync(fullPathWithBundles);
+//     console.log(fullPath + "  -----  " + hasBundles);
+//     if (hasBundles) {
+//       modsWithBundles.push(fullPathWithBundles);
+//     }
+//   }
+// });
+
+console.log("\nTotal Number of Mods Currently Installed : " + mods.length);
+console.log("\n");
